1bd701d72771d65548e4f6f80858d2efa0a6d82d
Aron Jones <aron.jones@gmail.com>
2014-02-05 10:20:13 -0500

    back to bash

--- lib/jump.dart
+++ lib/jump.dart
@@ -41,8 +41,8 @@ String generateCommand(List<String> arguments){
   }
 
   if(args.command == null){
-    return 'cd $JUMP_PATH/$jump_name 2>/dev/null;'
-    'or echo "No such jump point: $jump_name"';
+    return 'cd $JUMP_PATH/$jump_name 2>/dev/null'
+    '|| echo "No such jump point: $jump_name"';
 
   }else if (args.command.name == 'add'){
     return 'mkdir -p $JUMP_PATH; ln -s $jump_directory $JUMP_PATH/$jump_name;'
@@ -53,7 +53,7 @@ String generateCommand(List<String> arguments){
       'echo removed $jump_name from jump list';
 
   }else if (args.command.name == 'list'){
-    return 'ls -l $JUMP_PATH | sed \'s/  / /g\' | cut -d\' \' -f9- | sed \'s/ -/\t-/g\'; and echo';
+    return 'ls -l $JUMP_PATH | sed \'s/  / /g\' | cut -d\' \' -f9- | sed \'s/ -/\t-/g\' && echo';
 
   }else if (args.command.name == 'help'){
     return '''
